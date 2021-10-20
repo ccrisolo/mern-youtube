70da610f2b827c653c058f2414c9adf563145aa6
ccrisolo <ccrisolo3@gmail.com>
2021-10-20 15:31:10 -0700

    adds loading  of random  first video upon login

---
+++ src/components/Videos/VideoList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const VideoList = ({ videos }) => {
+    return videos.map((video, id) => (
+        <li style={{ listStyle: "none" }} key={id}>
+            <img alt='thumbnail' src={video.snippet.thumbnails.medium.url} />
+            <div>
+                <span>{video.snippet.title}</span>
+                <span>{video.snippet.channelTitle}</span>
+                <span>{video.snippet.tags}</span>
+            </div>
+        </li>
+    ));
+};
+
+export default VideoList;
--- src/pages/UserHomePage/UserHomePage.jsx
+++ src/pages/UserHomePage/UserHomePage.jsx
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
 import * as usersService from "../../utilities/users-service";
 import youtubeAPI from "../../utilities/youtube-api";
+import VideoList from "../../components/Videos/VideoList";
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const UserHomePage = () => {
-    const [videos, setVideos] = useState([]);
-    // const [selectedVideo, setSelectedVideo] = useState("");
-    // const [favorites, setFavorites] = useState([]);
+    const [popularVideos, setPopularVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+   
 
     useEffect(() => {
         const popularVideoList = async () => {
             const response = await youtubeAPI.get("videos", {
                 params: {
-                    part: ["snippet", "contentDetails", "statistics"],
+                    part: "snippet",
                     chart: "mostPopular",
                     regionCode: "US",
-                    maxResults: 8,
+                    maxResults: 10,
                     key: API_KEY,
                 },
-            })
-            console.log("response", response.data.items)
+            });
+            setPopularVideos(response.data.items);
         };
-        popularVideoList()
+        popularVideoList();
     }, []);
-  
+
+    useEffect(() => {
+        const loadFirstVideo = () => {
+            let firstVideo =
+                popularVideos[Math.floor(Math.random() * popularVideos.length)];
+                // console.log("firstVideo", !firstVideo ? '' : firstVideo.id)
+            setSelectedVideo(!firstVideo ? '' : firstVideo.id);
+        };
+        loadFirstVideo();
+    }, [popularVideos]);
+
+    // console.log("popularVideos", popularVideos);
+    
 
     async function handleCheckToken() {
         const expDate = await usersService.checkToken();
@@ -32,12 +45,27 @@ const UserHomePage = () => {
     }
 
     return (
-        <>
+        <div>
             <h1>User Home Page</h1>
             <button onClick={handleCheckToken}>
                 Check When My Login Expires
             </button>
-        </>
+            <div>
+                <iframe
+                    frameBorder='0'
+                    height='400px'
+                    width='700px'
+                    title='Video Player'
+                    src={`https://www.youtube-nocookie.com/embed/${selectedVideo}`}
+                />
+            </div>
+            <h2>Popular Videos</h2>
+            <div>
+                <ul>
+                    <VideoList videos={popularVideos} />
+                </ul>
+            </div>
+        </div>
     );
 };
 
