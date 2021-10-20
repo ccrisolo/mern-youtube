048187ad1d7fcda1a048cc3d632e4f67cb65d6b1
ccrisolo <ccrisolo3@gmail.com>
2021-10-20 15:46:32 -0700

    updates style on popular videos

--- src/components/Videos/VideoList.jsx
+++ src/components/Videos/VideoList.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const VideoList = ({ videos }) => {
     return videos.map((video, id) => (
-        <li style={{ listStyle: "none" }} key={id}>
+        <li style={{ listStyle: "none" , margin: '20px'}} key={id}>
             <img alt='thumbnail' src={video.snippet.thumbnails.medium.url} />
-            <div style={{width: '400px'}}>
+            <div style={{width: '300px' }}>
                 <span>{video.snippet.title}</span>
                 <span>{video.snippet.channelTitle}</span>
                 <span>{video.snippet.tags}</span>
