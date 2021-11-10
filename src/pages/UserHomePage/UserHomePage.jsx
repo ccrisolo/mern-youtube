098df9ca92cf2b3ec158c3ab879bba2848273b77
ccrisolo <ccrisolo3@gmail.com>
2021-11-10 12:42:34 -0800

    adds video title under video player

--- src/pages/UserHomePage/UserHomePage.jsx
+++ src/pages/UserHomePage/UserHomePage.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import * as usersService from "../../utilities/users-service";
 import youtubeAPI from "../../utilities/youtube-api";
 import VideoList from "../../components/Videos/VideoList";
+import "./UserHomePage.css";
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const UserHomePage = () => {
     const [popularVideos, setPopularVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [selectedVideoTitle, setSelectedVideoTitle] = useState(null);
 
     //handleSearch
     //input takes in a string
@@ -17,6 +19,8 @@ const UserHomePage = () => {
     function handleSelectedVideo(video) {
         //takes selected thumbnail id and sets it to selectedVideo
         // console.log("video.id", video.id);
+        console.log("video", video.snippet.title);
+        setSelectedVideoTitle(video.snippet.title);
         setSelectedVideo(video.id);
     }
 
@@ -41,6 +45,7 @@ const UserHomePage = () => {
             let firstVideo =
                 popularVideos[Math.floor(Math.random() * popularVideos.length)];
             setSelectedVideo(!firstVideo ? "" : firstVideo.id);
+            setSelectedVideoTitle(!firstVideo ? "" : firstVideo.snippet.title);
         };
         loadFirstVideo();
     }, [popularVideos]);
@@ -51,25 +56,28 @@ const UserHomePage = () => {
     //     const expDate = await usersService.checkToken();
     //     return console.log(expDate);
     // }
-
     return (
-        <div style={{ display: "flex", marginTop: "30px", marginLeft: "2%" }}>
-            <iframe
-                allowFullScreen
-                frameBorder='0'
-                height='700px'
-                width='11000px'
-                title='Video Player'
-                src={`https://www.youtube-nocookie.com/embed/${selectedVideo}`}
-            />
-            <ul>
-                <span style={{ fontWeight: "600" }}>Popular Videos</span>
-                <VideoList
-                    videos={popularVideos}
-                    handleSelectedVideo={handleSelectedVideo}
-                    setSelectedVideo={setSelectedVideo}
+        <div className='page-wrapper'>
+            <div className='iframe-wrapper'>
+                <iframe
+                    allowFullScreen
+                    frameBorder='0'
+                    height='100%'
+                    title='Video Player'
+                    src={`https://www.youtube-nocookie.com/embed/${selectedVideo}`}
                 />
-            </ul>
+                <h2>{selectedVideoTitle}</h2>
+            </div>
+            <div className='list-wrapper'>
+                <ul>
+                    <h2>Popular Videos</h2>
+                    <VideoList
+                        videos={popularVideos}
+                        handleSelectedVideo={handleSelectedVideo}
+                        setSelectedVideo={setSelectedVideo}
+                    />
+                </ul>
+            </div>
         </div>
     );
 };
